54a527e15d83b9d08836fb2551cf10348c66621d
Dinesh Pelandapathirage <zyon@zyonstudios.co.uk>
2022-04-26 20:26:42 +0100

    React auth form example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class Contact extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        password: 'react@auth',
+        authorized: false
+      };
+      this.authorize = this.authorize.bind(this);
+    }
+  
+    authorize(e) {
+      const password = e.target.querySelector(
+        'input[type="password"]').value;
+      const auth = password === this.state.password;
+      this.setState({
+        authorized: auth
+      });
+    }
+  
+    render() {
+      const login =(
+        <form action='#' onSubmit={this.authorize}>
+        <input type="password" placehoder="Password" />
+        <input type="submit"/>
+        </form>
+      );
+      const contactInfo =(
+          <>
+            <div className='mouse-img'>
+                <img 
+                src='images/mouse.jpg'
+                alt='The mouse'
+                width='100px' 
+                height='auto' />
+            </div>
+        <ul>
+            <li>
+              Hello ! 
+            </li>
+            <li>
+              Thank you for entering the correct passowrd and exposed me.
+            </li>
+          </ul>
+        </>
+      );
+      return (
+          <>
+          <div className='heading'>
+              <h2>Authorization example in React</h2>
+              <p>This is an example to handle authorisation. Please type the password 'react@auth' to see the hidden details </p>
+          </div>
+          <div className='container'>            
+            <div id="authorization" >
+                <h1>{this.state.authorized ? 'You got it !' : 'Enter the Password'}</h1>  
+                {this.state.authorized ? contactInfo : login}
+            </div>            
+          </div>
+            
+        </>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Contact />, 
+    document.getElementById('root')
+  );
